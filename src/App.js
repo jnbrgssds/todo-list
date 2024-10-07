2944ca9aa7fc164cc8c457144eb0b27e12d6373e
jean <jeanborges1996@gmail.com>
2024-10-06 22:02:19 -0300

    adicionar tailwind

--- src/App.js
+++ src/App.js
@@ -18,22 +18,38 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1>Lista de Tarefas</h1>
-      <input
-        type="text"
-        value={task}
-        onChange={(e) => setTask(e.target.value)}
-        placeholder="Adicione uma nova tarefa"
-      />
-      <button onClick={addTask}>Adicionar</button>
-      <ul>
-        {tasks.map((t, index) => (
-          <li key={index}>
-            {t} <button onClick={() => removeTask(index)}>Remover</button>
-          </li>
-        ))}
-      </ul>
+    <div className="bg-stone-950 min-h-screen flex items-center justify-center">
+      <div className="bg-stone-600 shadow-lg rounded-lg p-6 w-96">
+        <h1 className="text-2xl font-bold text-center mb-4">Lista de Tarefas</h1>
+        <div className="mb-4">
+          <input
+            type="text"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+            placeholder="Adicione uma nova tarefa"
+            className="border border-gray-300 rounded w-full p-2 mb-2 focus:outline-none focus:border-blue-500"
+          />
+          <button
+            onClick={addTask}
+            className="bg-amber-800 text-white px-4 py-2 rounded w-full hover:bg-blue-600"
+          >
+            Adicionar
+          </button>
+        </div>
+        <ul className="space-y-2">
+          {tasks.map((t, index) => (
+            <li key={index} className="bg-gray-200 p-2 rounded flex justify-between">
+              {t}
+              <button
+                onClick={() => removeTask(index)}
+                className="text-red-500 hover:text-red-700"
+              >
+                Remover
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
